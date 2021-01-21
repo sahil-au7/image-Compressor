7a58d654306135bf648345c967729ac9001a3e63
sahil-au7 <sahilyadav601@gmail.com>
2021-01-22 00:43:03 +0530

    almost done with the build

--- src/Components/ImageCompressor.js
+++ src/Components/ImageCompressor.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./ImageCompressor.css";
-import Resizer from "react-image-file-resizer";
+import imageCompression from "browser-image-compression";
 import Button from "@material-ui/core/Button";
 import CircularSlider from "@fseehawer/react-circular-slider";
 
 const ImageCompressor = () => {
   const [image, setImage] = useState("");
-  const [file, setFile] = useState("");
   const [newImage, setNewImage] = useState("");
+  const [newFile, setNewFile] = useState("");
+  const [file, setFile] = useState("");
   const [quality, setQuality] = useState("");
 
   const handelFile = (event) => {
@@ -15,69 +16,90 @@ const ImageCompressor = () => {
     setFile(event.target.files[0]);
   };
 
-  const reSizeImage = () => {
-    var fileInput = false;
-    if (file) {
-      fileInput = true;
-    }
-    if (fileInput) {
-      try {
-        Resizer.imageFileResizer(
-          file,
-          300,
-          300,
-          "JPEG",
-          quality,
-          0,
-          (uri) => {
-            setNewImage(uri);
-          },
-          "base64",
-          200,
-          200
-        );
-      } catch (err) {
-        console.log(err);
-      }
+  const reSizeImage = async () => {
+    const options = {
+      maxSizeMB: quality / 1024,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
+    try {
+      const compressedFile = await imageCompression(file, options);
+      setNewImage(URL.createObjectURL(compressedFile));
+      setNewFile(compressedFile);
+    } catch (error) {
+      console.log(error);
     }
   };
   return (
     <div className="imageCompressor">
       <div className="imageCompressor__upload">
-        <div className="imageCompressor__container">
-          <img className="imageCompressor__image" src={image} alt="" />
-        </div>
-        <input accept="image/*" onChange={handelFile} type="file" />
-      </div>
-      <div className="imageCompressor__circularSlider">
-        <CircularSlider
-          label="reSize %"
-          width={150}
-          labelColor=""
-          progressSize={20}
-          trackSize={24}
-          dataIndex={0}
-          min={0}
-          labelFontSize="1rem"
-          valueFontSize="1.5rem"
-          max={100}
-          direction="1"
-          onChange={(value) => setQuality(value)}
-        />
-        <Button onClick={reSizeImage} variant="contained" color="primary">
-          Resize
+        {image ? (
+          <div className="imageCompressor__container">
+            <img className="imageCompressor__image" src={image} alt="" />
+          </div>
+        ) : (
+          <div></div>
+        )}
+
+        {file ? (
+          <p className="imageCompressor__sizeInfo">
+            {" "}
+            Original Size : {(file.size / 1024 / 1024).toFixed(3)} MB
+          </p>
+        ) : (
+          <p></p>
+        )}
+        <Button variant="contained" color="primary">
+          <input
+            className="imageCompressor__chooseFile"
+            accept="image/*"
+            onChange={handelFile}
+            type="file"
+          ></input>
         </Button>
       </div>
-      <div className="compressed__image">
-        <div className="imageCompressor__container">
-          <img className="imageCompressor__image" src={newImage} alt="" />
-        </div>
-        <a href={newImage} download>
-          <Button variant="contained" color="primary">
-            Download
+      {image ? (
+        <div className="imageCompressor__circularSlider">
+          <CircularSlider
+            label="reSize %"
+            width={150}
+            labelColor=""
+            progressSize={20}
+            trackSize={24}
+            dataIndex={0}
+            min={0}
+            labelFontSize="1rem"
+            valueFontSize="1.5rem"
+            max={100}
+            direction="1"
+            onChange={(value) => setQuality(value)}
+          />
+          <Button onClick={reSizeImage} variant="contained" color="primary">
+            Resize
           </Button>
-        </a>
-      </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {newImage ? (
+        <div className="compressed__image">
+          <div className="imageCompressor__container">
+            <img className="imageCompressor__image" src={newImage} alt="" />
+          </div>
+          {newFile ? (
+            <p>New Size : {(newFile.size / 1024 / 1024).toFixed(3)} MB</p>
+          ) : (
+            <p></p>
+          )}
+          <a href={newImage} download>
+            <Button variant="contained" color="primary">
+              Download
+            </Button>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
